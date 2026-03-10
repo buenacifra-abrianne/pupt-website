53e4a05c081d16227fce8223a546498c7ce311a7
Bernas-Ernesto <thonbernas1134@gmail.com>
2026-03-10 18:55:57 +0800

    rename footer 1998 to 1992

--- resources/views/components/landing-footer.blade.php
+++ resources/views/components/landing-footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="landing-footer">
-    <span class="footer-copy">&copy; 1998-{{ now()->year }} Polytechnic University of the Philippines</span>
+    <span class="footer-copy">&copy; 1992-{{ now()->year }} Polytechnic University of the Philippines</span>
     <span class="footer-sep" aria-hidden="true">|</span>
     <a href="https://www.pup.edu.ph/terms/" target="_blank" rel="noopener noreferrer">Terms of Use</a>
     <span class="footer-sep" aria-hidden="true">|</span>
