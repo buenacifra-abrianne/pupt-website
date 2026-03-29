e0aff74ed76969e2391f2db23a8c890b558240d3
Abrianne <abriannebuenacifra@gmai.com>
2026-03-30 02:11:18 +0800

    add api key verification

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AnnouncementApiController;
 
-Route::prefix('announcements')->group(function () {
-    Route::get('/list', [AnnouncementApiController::class, 'list']);
-    Route::get('/{announcement_id}', [AnnouncementApiController::class, 'show']);
-});
+Route::prefix('announcements')
+    ->middleware('idp.apikey')
+    ->group(function () {
+        Route::get('/list', [AnnouncementApiController::class, 'list']);
+        Route::get('/{announcement_id}', [AnnouncementApiController::class, 'show']);
+    });
