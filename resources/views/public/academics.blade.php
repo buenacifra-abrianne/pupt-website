ce42c2194cfa3b2d1db82cf2264d2dbf43cb0648
Precious Hannah Corpus <corpus.precioushannah1@gmail.com>
2026-03-31 15:24:39 +0800

    add 'see more' button in content cards

--- resources/views/public/academics.blade.php
+++ resources/views/public/academics.blade.php
@@ -6,7 +6,7 @@
     <title>Academics - Polytechnic University of the Philippines</title>
     <link rel="stylesheet" href="{{ asset('assets/styles/layout.css') }}?v={{ filemtime(public_path('assets/styles/layout.css')) }}">
     <link rel="stylesheet" href="{{ asset('assets/css/academics.css') }}?v={{ filemtime(public_path('assets/css/academics.css')) }}">
-    <link rel="icon" type="image/png" href="../assets/static_img/logo.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="{{ asset('assets/static_img/logo.png') }}" sizes="32x32">
 </head>
 <body>
     <!-- Header -->
@@ -53,7 +53,7 @@
                     <p class="section-tag">Contents</p>
                 </div>
 
-                <nav class="contents-cards" aria-label="Academic page contents">
+                <nav class="contents-cards show-card-action" aria-label="Academic page contents">
                     <a href="{{ route('public.degree-programs') }}" class="contents-card card_without_section">
                         <div class="contents-card-inner">
                             <div class="contents-card-front">
@@ -193,7 +193,7 @@
     <!-- Footer -->
     <pup-footer></pup-footer>
 
-    <script src="../assets/js/script.js" defer></script>
+    <script src="{{ asset('assets/js/script.js') }}" defer></script>
     <script src="{{ asset('assets/js/pup-components.js') }}?v={{ filemtime(public_path('assets/js/pup-components.js')) }}" defer></script>
 </body>
 </html>
