bf9e1352c06329ce2c3d2704b6ac100016d6f6fc
Abrianne <abriannebuenacifra@gmai.com>
2026-04-23 17:25:55 +0800

    integrate oneportal

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -94,7 +94,7 @@
         <!-- Buttons -->
         <div class="button-container">
 
-            <a href="{{ route('oneportal.redirect') }}" class="portal-button">
+            <a href="{{ rtrim(config('services.oneportal.url'), '/') }}" class="portal-button">
                 <span class="icon">
                     <!-- users -->
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
@@ -102,19 +102,19 @@
 </svg>
 
                 </span>
-                <span class="text">Faculty/Admin</span>
+                <span class="text">OnePortal</span>
             </a>
 
-            <a href="{{ route('oneportal.redirect') }}" class="portal-button">
+            <!-- <a href="{{ route('oneportal.redirect') }}" class="portal-button">
                 <span class="icon">
-                    <!-- academic-cap -->
+
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
 </svg>
 
                 </span>
                 <span class="text">Student</span>
-            </a>
+            </a> -->
 
             <a href="{{ route('public.home') }}" class="portal-button">
                 <span class="icon">
@@ -124,7 +124,7 @@
 </svg>
 
                 </span>
-                <span class="text">Visitor</span>
+                <span class="text">View Homepage</span>
             </a>
 
         </div>
