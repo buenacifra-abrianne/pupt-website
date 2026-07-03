d21b587e4ede8c0bc3d7decec32084f1873cc647
Buenacifra, Abrianne <abriannebuenacifra@gmail.com>
2026-07-03 12:26:24 +0800

    add new login button

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -98,6 +98,18 @@
             @endif
 
             <div class="button-container">
+                <a href="{{ rtrim(config('services.puptweb.url'), '/') }}" class="portal-button portal-button--primary">
+                    <span class="icon" aria-hidden="true">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                        </svg>
+                    </span>
+                    <span class="text">
+                        <strong>Login</strong>
+                        <small>Faculty and staff sign in</small>
+                    </span>
+                </a>
+
                 <a href="{{ rtrim(config('services.oneportal.url'), '/') }}" class="portal-button portal-button--primary">
                     <span class="icon" aria-hidden="true">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
@@ -106,7 +118,7 @@
                     </span>
                     <span class="text">
                         <strong>One Portal</strong>
-                        <small>Faculty and staff sign in</small>
+                        <small>Access all systems within the PUP-Taguig Campus</small>
                     </span>
                 </a>
 
