46fa970b0f58e2e5000d7ca10bc81aa39ad9d048
Buenacifra, Abrianne <abriannebuenacifra@gmail.com>
2026-07-08 12:08:31 +0800

    remove password field

--- resources/views/superadmin/login.blade.php
+++ resources/views/superadmin/login.blade.php
@@ -51,15 +51,7 @@
                                 </div>
                             </div>
 
-                            <div class="form-group">
-                                <label for="password" class="sr-only">Password</label>
-                                <div class="input-wrapper">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="1.8" stroke="currentColor" class="input-icon" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
-                                    </svg>
-                                    <input type="password" id="password" name="password" placeholder="Password" required>
-                                </div>
-                            </div>
+
                             @if(session('logged_out'))
                                 <div class="logout-message">
                                     You have been successfully logged out.
