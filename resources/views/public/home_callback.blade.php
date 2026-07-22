e5f055a1d989e5f7c377721d7f1f7703d7ea9146
Bernas-Ernesto <thonbernas1134@gmail.com>
2026-07-22 17:42:38 +0800

    refractor callback ui

--- resources/views/public/home_callback.blade.php
+++ resources/views/public/home_callback.blade.php
@@ -221,6 +221,5 @@
             resetAndRedirect();
         });
     </script>
-<script src="{{ asset('assets/js/widget-dock.js') }}?v={{ filemtime(public_path('assets/js/widget-dock.js')) }}" defer></script>
 </body>
 </html>
