31dcf897d3d0630d658df4e6535bbed62f4bda3e
Buenacifra, Abrianne <abriannebuenacifra@gmail.com>
2026-07-28 15:01:47 +0800

    remove login button in index page

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -116,19 +116,7 @@
             @endif
 
             <div class="button-container">
-                @if ($isIdpOnline ?? true)
-                    <a href="{{ rtrim(config('services.idp.base_url'), '/') . '/api/v1/auth/authorize?client_id=' . urlencode(config('services.idp.client_id')) }}" class="portal-button portal-button--primary">
-                        <span class="icon" aria-hidden="true">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
-                            </svg>
-                        </span>
-                        <span class="text">
-                            <strong>Login</strong>
-                            <small>Faculty and staff sign in</small>
-                        </span>
-                    </a>
-                @else
+                @if (!($isIdpOnline ?? true))
                     <a href="{{ route('superadmin.login') }}" class="portal-button portal-button--primary">
                         <span class="icon" aria-hidden="true">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
